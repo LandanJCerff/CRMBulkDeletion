bc2c07f107fa383cdd4db9154c067057167f4f25
Landan Cerff <41990488+LandanJCerff@users.noreply.github.com>
2018-08-03 13:10:45 +0100

    Update Program.cs

--- CRMBulkDeletion/Program.cs
+++ CRMBulkDeletion/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Client;
 using System;
 using System.Net;
@@ -9,26 +9,23 @@ using System.Threading;
 namespace CRMBulkDeletion
 {
     class Program : ServiceLayer
-    {
-              
+    {              
         public static void Main(string[] args)
-        {
-           
+        {           
             IOrganizationService _orgServ = null;
-
+            string env = args[0];
             try
             {
                 ClientCredentials clientCredentials = new ClientCredentials();
                 clientCredentials.UserName.UserName = "";
                 clientCredentials.UserName.Password = "";
 
-                Console.WriteLine("credentials set");
+                 Console.WriteLine("credentials set");
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-                _orgServ = (IOrganizationService)new OrganizationServiceProxy(new Uri("https:/.....api.crm4.dynamics.com/XRMServices/2011/Organization.svc"),
+                _orgServ = (IOrganizationService)new OrganizationServiceProxy(
+                    new Uri("https://shgl-" + env + ".api.crm4.dynamics.com/XRMServices/2011/Organization.svc"),
                     null, clientCredentials, null);
-
-               
-
+                              
                 Console.WriteLine("connection made");
             }
             catch (FaultException<OrganizationServiceFault> e)
@@ -48,7 +45,6 @@ namespace CRMBulkDeletion
             {               
                 Console.WriteLine(e.Message);
             }
-        }
-   
+        }   
     }
 }
